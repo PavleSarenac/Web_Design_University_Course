eefdd265627167d8f171346b4bf5a1613599d26a
Pavle Šarenac <sarenac.pavle@gmail.com>
2023-05-18 01:32:43 +0200

    V - 9 (Vue.js)

---
+++ Vezbe 9/myapp/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1 :class="h1class">Welcome to our online bookstore {{bookStoreName}}</h1>
+    <br>
+    <button class="btn btn-success">Sign in</button>
+  </div>
+</template>
+
+<style>
+  .extraClass {
+    color: darkgreen
+  }
+</style>
+
+<script>
+  export default {
+    name: "Home",
+    data() {
+      return {
+        bookStoreName: "Booka",
+        h1class: "extraClass"
+      }
+    }
+  }
+</script>
